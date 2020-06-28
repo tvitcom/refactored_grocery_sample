a0450c66edd3bd7c199bc004f05b85baa11781af
Dwi Wahyudi <dwi.wahyudi.14@gmail.com>
2020-06-28 17:11:06 +0700

    add db_util for connecting with db

--- internal/app/grocery/webapi/route.go
+++ internal/app/grocery/webapi/route.go
@@ -1,6 +1,10 @@
 package webapi
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/dwahyudi/golang_grocery_sample/internal/app/grocery/webhandler/shopping_list_handler"
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+)
 
 func Route(r *gin.Engine) *gin.Engine {
 	r.GET("/ping", func(c *gin.Context) {
@@ -8,6 +12,14 @@ func Route(r *gin.Engine) *gin.Engine {
 			"message": "pong",
 		})
 	})
+
+	v1ShoppingList := r.Group("/v1/shopping-list")
+	{
+		v1ShoppingList.POST("/", shopping_list_handler.CreateHandler)
+		v1ShoppingList.GET("/:id", shopping_list_handler.ShowHandler)
+		v1ShoppingList.PUT("/:id", shopping_list_handler.PutHandler)
+		v1ShoppingList.DELETE("/:id", shopping_list_handler.DeleteHandler)
+	}
 
 	return r
 }
