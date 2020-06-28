9769e35e9996087ed7693cb2b157f50d6219a732
Dwi Wahyudi <dwi.wahyudi.14@gmail.com>
2020-06-28 18:02:14 +0700

    remove omitempty validators in struct

--- internal/app/grocery/types/shopping_list.go
+++ internal/app/grocery/types/shopping_list.go
@@ -2,7 +2,7 @@ package types
 
 type ShoppingList struct {
 	Id   int64  `json:"id"`
-	Name string `json:"name" binding:"required,omitempty"`
-	Qty  int    `json:"qty" binding:"required,gte=0,omitempty"`
-	Unit string `json:"unit" binding:"required,omitempty"`
+	Name string `json:"name" binding:"required"`
+	Qty  int    `json:"qty" binding:"required,gte=0"`
+	Unit string `json:"unit" binding:"required"`
 }
